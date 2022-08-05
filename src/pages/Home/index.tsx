caf881d88e83133cf5b8e3519175b71bd3ad70c0
Felipe Ribeiro <piporibeiro@gmail.com>
2022-08-05 18:38:35 -0300

    feat: create history page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ import {
   FormContainer,
   CountdownContainer,
   Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
 } from './styles'
 
 export const Home = () => {
@@ -12,9 +15,25 @@ export const Home = () => {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id={'task'} />
+          <TaskInput
+            id={'task'}
+            list={'task-suggestions'}
+            placeholder={'Dê um nome para o seu projeto'}
+          />
+          <datalist id={'task-suggestions'}>
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
           <label htmlFor="minutesAmount">Durante</label>
-          <input type={'number'} id={'minutesAmount'} />
+          <MinutesAmountInput
+            type={'number'}
+            id={'minutesAmount'}
+            placeholder={'00'}
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos.</span>
         </FormContainer>
         <CountdownContainer>
@@ -25,10 +44,10 @@ export const Home = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
-          <Play />
+        <StartCountdownButton type="submit">
+          <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
